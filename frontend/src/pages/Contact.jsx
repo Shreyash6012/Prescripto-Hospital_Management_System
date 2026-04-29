73bf8c082d6294c61859e09089695a37b60106cd
Shreyash <shreyashghadge.6012@gmail.com>
2026-04-29 11:14:17 +0530

    changes

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-lg text-gray-600'>Our OFFICE</p>
           <p className='text-gray-500'>54709 willms Station <br />Suite 350,washington ,USA</p>
-          <p className='text-gray-500'>Tel:8010059132<br/>Email:shivaandmana@gmail.com</p>
+          <p className='text-gray-500'>Tel:8010059132<br/>Email:rohit&Shreyash12@gmail.com</p>
           <p className='font-semibold text-lg text-gray-600'>Careers at PRESCIPTO</p>
           <p className='text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500 '>Explore Jobs</button>
